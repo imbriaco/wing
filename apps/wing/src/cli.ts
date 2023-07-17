9a70ff57a5edba7b936ca9509c7777be019424c8
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-07-17 10:41:03 +0200

    adding test --watch to wing cli

--- apps/wing/src/cli.ts
+++ apps/wing/src/cli.ts
@@ -9,6 +9,8 @@ import { collectCommandAnalytics } from "./analytics/collect";
 import { exportAnalytics } from "./analytics/export";
 import { optionallyDisplayDisclaimer } from "./analytics/disclaimer";
 import { currentPackage } from "./util";
+import { log } from "console";
+import { chokidar } from "chokidar";
 
 export const PACKAGE_VERSION = currentPackage.version;
 let analyticsExportFile: Promise<string | undefined>;
@@ -100,6 +102,26 @@ async function main() {
     }
   }
 
+  async function watchHook(cmd: Command) {
+    const entrypoint = cmd.args[0];
+    const watch = cmd.opts().watch;
+
+    if (watch) {
+      const watcher = chokidar.watch(entrypoint, {
+        ignoreInitial: true,
+      });
+
+      watcher.on("change", async () => {
+        console.log("Wing source changed, rerunning tests...");
+        runSubCommand("test");
+      });
+
+      // watcher.on("error", (error) => {
+      //   console.error("Error watching Wing source files", error);
+      // });
+    }
+  }
+
   async function updateHook(cmd: Command) {
     const updateCheck = cmd.opts().updateCheck;
     if (updateCheck) {
@@ -152,7 +174,9 @@ async function main() {
         .default("sim")
     )
     .option("-p, --plugins [plugin...]", "Compiler plugins")
+    .option("-w, --watch", "Watch Wing source files for changes and rerun tests")
     .hook("preAction", progressHook)
+    .hook("preAction", watchHook)
     .hook("preAction", collectAnalyticsHook)
     .action(runSubCommand("test"));
 
